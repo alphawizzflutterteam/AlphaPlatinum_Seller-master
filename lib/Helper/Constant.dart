afe1f818b7659c691506e486269d55c589479245
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-07-10 16:06:20 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,21 @@
+final String appName = ' Alpha Platinum Seller';
+
+final String packageName = 'com.alphaPlatinum.seller';
+
+final String iosPackage = 'com.alphaPlatinum.seller';
+
+
+
+final String baseUrl = 'https://alphasilver.productsalphawizz.com/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
